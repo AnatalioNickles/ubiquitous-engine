7c363bf6a4ad4647a0cc2525d52fc5b18a1fc389
broki1 <77469429+broki1@users.noreply.github.com>
2022-06-01 11:25:24 -0400

    Add files via upload

---
+++ src/main/java/com/capstone/bank/model/BankAccount.java
@@ -0,0 +1,73 @@
+package com.capstone.bank.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+public class BankAccount {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "account_number")
+	private Long accountNumber;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "balance")
+	private double balance;
+	
+	public BankAccount() {
+		
+	}
+
+	public BankAccount(String firstName, String lastName, double balance) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.balance = balance;
+	}
+
+
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+}
